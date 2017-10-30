77ca86dd9a834264a4ff788697093015fab3c988
monzag <zaganiacz.m@gmail.com>
2017-10-30 14:08:00 +0100

    Add setUp, tests for getters and empty constructor

--- src/test/java/com/monzag/checkpoint4Test/SaleTest.java
+++ src/test/java/com/monzag/checkpoint4Test/SaleTest.java
@@ -1,8 +1,48 @@
 package com.monzag.checkpoint4Test;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SaleTest {
+
+    private Sale sale;
+
+    @BeforeEach
+    public void setUp() {
+        this.sale = new Sale("CookBook", "25.55", 23);
+    }
+
+    @Test
+    public void testEmptyConstructor() {
+        assertNotNull(new Sale());
+    }
+
+    @Test
+    public void testGetProductName() {
+        String expected = "CookBook";
+        String actual = sale.getProductName();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetTaxRate() {
+        Integer expected = 23;
+        Integer actual = sale.getTaxRate();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetNetValue() {
+        BigDecimal expected = new BigDecimal(25.55);
+        BigDecimal actual = sale.getNetValue();
+        assertEquals(expected, actual);
+    }
+
+
 
 
 
